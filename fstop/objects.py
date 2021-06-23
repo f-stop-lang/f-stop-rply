695312b2be0a3004eadad51648746826a2add4db
Tom-the-Bomb <thello522@gmail.com>
2021-06-23 10:01:27 -0400

    sus

--- fstop/objects.py
+++ fstop/objects.py
@@ -6,14 +6,14 @@ class ImageRepr(BaseBox):
 
     def __init__(self, image):
         self.image = image
-        self.array = cv.imdecode(np.asarray(self.image), cv.IMREAD_COLOR)
+        self.array = np.array(self.image)
     
     def __repr__(self):
         return "<ImageRepr image='%s'>" % self.image
 
     def __getattribute__(self, attr: str):
         if attr == 'array':
-            self.array = arr = cv.imdecode(np.asarray(self.image), cv.IMREAD_COLOR)
+            self.array = arr = np.array(self.image)
             return arr
         else:
             return super().__getattribute__(attr)
