9cdbb414a11d0a59697fc3b3611364d7826f4785
Tom-the-Bomb <thello522@gmail.com>
2021-06-23 09:27:20 -0400

    tst

--- fstop/objects.py
+++ fstop/objects.py
@@ -1,18 +1,19 @@
 import numpy as np
+import cv2 as cv
 from rply.token import BaseBox
 
 class ImageRepr(BaseBox):
 
     def __init__(self, image):
         self.image = image
-        self.array = np.asarray(self.image)
+        self.array = cv.imdecode(np.asarray(self.image), cv.IMREAD_COLOR)
     
     def __repr__(self):
         return "<ImageRepr image='%s'>" % self.image
 
     def __getattribute__(self, attr: str):
         if attr == 'array':
-            self.array = arr = np.asarray(self.image)
+            self.array = arr = cv.imdecode(np.asarray(self.image), cv.IMREAD_COLOR)
             return arr
         else:
             return super().__getattribute__(attr)
