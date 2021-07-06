3b9c09a2d6489c309d41b40e39c224b5c4c11a54
Tom-the-Bomb <thello522@gmail.com>
2021-07-06 11:28:32 -0400

    fixed tests

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@
 from fstop import Runner
 
 if __name__ == '__main__':
-    with open("test.fstop") as ft:
+    with open("tests/test.fstop") as ft:
         string = ft.read()
 
     run = Runner()
